ac2c0f6cd8d24c67ef3bc3bbc2c5a037d1b2fa3b
Reon Saji <reonsajinedz@gmail.com>
2018-10-16 23:47:03 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,4 +1,4 @@
-#ZipCar-" Wheels When You Wan;ot Themslwepo"
+#ZipCar-" Wheels When You Wa;ot Themslwepo"
 carinhand = ['Duster', 'Audi', 'Duster','auto']
 totalcar = ['Ford', 'uster', 'tyio', 'Audi','Kiwi', 'Jaguar']
 carinrent = ['Ford', 'Celerio']
